d93089fe15adf4414ea1b5251dda6be8d69a7272
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 08:01:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL 
-      CREATE TABLE IF NOT EXISTS student(
+      CREATE TABLE IF NOT EXISTS students(
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
