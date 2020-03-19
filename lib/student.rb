bf8e4d535339fde258ab9bc7ec5766ab7665f914
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 08:07:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,11 +44,15 @@ class Student
       SQL
       DB[:conn].execute(sql,self.name,self.grade)
       @id =DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
     
   end
   
   def self.create(name,grade)
+    student
+    sql = <<-SQL
     
+    SQL
   end
 
 
