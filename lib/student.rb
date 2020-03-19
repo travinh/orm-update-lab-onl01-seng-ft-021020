80a6f4c579778f008e77306a2eca277be671a21b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 07:59:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,27 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS student(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+    
+  end
 
 
 end
