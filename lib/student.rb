a29bf83ce48041b555b73da4581a00b5607ae59e
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 08:06:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,13 +36,14 @@ class Student
   
   def save
     if self.id 
-      update
-    sql = <<-SQL
-      INSERT INTO students (name,grade)
-      VALUES (?,?)
-    SQL
-    DB[:conn].execute(sql,self.name,self.grade)
-    @id =DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name,grade)
+        VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      @id =DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     
   end
   
