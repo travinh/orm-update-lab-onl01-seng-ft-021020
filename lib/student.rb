9e100e7a2015a263263ded059e9529f4e35c9e88
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 08:07:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,10 @@ class Student
     
   end
   
-  def self.create(name,grade)
+  def self.create(name:,grade:)
+    student =  Student.new(name,grade)
+    student.save
     student
-    sql = <<-SQL
-    
-    SQL
   end
 
 
